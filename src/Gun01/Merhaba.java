ffe8588aad8178000a2b41828e87128b1380b688
TechnoStudy <ismettechno@gmail.com>
2022-10-11 07:51:06 +0100

    mesajda düzeltme yapıldı

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -2,7 +2,7 @@ package Gun01;
 
 public class Merhaba {
     public static void main(String[] args) {
-        System.out.println("merhaba ");
+        System.out.println("merhaba dünya");
 
         for (int i = 0; i <5 ; i++) {
             System.out.println("Merhaba "+i);
